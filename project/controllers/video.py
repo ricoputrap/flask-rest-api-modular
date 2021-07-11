973734f38cbd59a01bcccae752e7b60e8cd92991
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-07-11 20:07:31 +0700

    Implement function to create a new video (put)

--- project/controllers/video.py
+++ project/controllers/video.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse, abort, fields, marshal_with
 from project.models.video import VideoModel
+from project.db import db
 
 # Arguments definition for PUT operation (create video object)
 video_put_args = reqparse.RequestParser()
@@ -17,10 +18,20 @@ resource_fields = {
 class Video(Resource):
   @marshal_with(resource_fields)
   def get(self, video_id):
-    print("VIDEO CONTROLLER - GET")
     result = VideoModel.query.filter_by(id=video_id).first()
     if not result:
       abort(404, message="Could not find video with that id...")
     return result
+  
+  @marshal_with(resource_fields)
+  def put(self, video_id):
+    args = video_put_args.parse_args()
+    result = VideoModel.query.filter_by(id=video_id).first()
+    if result:
+      abort(409, message="The video id has been taken...")
     
+    video = VideoModel(id=video_id, name=args["name"], views=args["views"], likes=args["likes"])
+    db.session.add(video)
+    db.session.commit()
     
+    return video, 201
