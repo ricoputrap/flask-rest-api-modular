2a593e49fa7f33323765bf3aa4a6ca27b24c511d
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-08-08 00:48:11 +0700

    do migration for the new main & categorical tables

--- project/main.py
+++ project/main.py
@@ -8,6 +8,19 @@ from project.controllers.post import Post
 from project.controllers.todo import Todo
 import os
 
+# delete soon, must be called on controllers
+from project.models.main_tables.survey import SurveyModel
+from project.models.main_tables.survey_participant import SurveyParticipantModel
+from project.models.main_tables.email_template import EmailTemplateModel
+from project.models.main_tables.activity import ActivityModel
+from project.models.main_tables.survey_blast import SurveyBlastModel
+from project.models.categorical_tables.survey_status import SurveyStatusModel
+from project.models.categorical_tables.activity_type import ActivityTypeModel
+from project.models.categorical_tables.survey_channel import SurveyChannelModel
+from project.models.categorical_tables.survey_blast_status import SurveyBlastStatusModel
+
+
+
 def init_app():
   '''Flask Project Application Factory'''
   app = Flask(__name__)
